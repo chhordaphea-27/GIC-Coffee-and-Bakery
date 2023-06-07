ddafc659f3a88f7af052a5ca6e28348e1bd4b3d7
chhordaphea-27 <106377148+chhordaphea-27@users.noreply.github.com>
2023-06-07 14:58:14 +0700

    Done table management

--- src/main/java/com/gic_coffee_and_bakery/softwareeginerringgroup13/Controller/TableManagementController.java
+++ src/main/java/com/gic_coffee_and_bakery/softwareeginerringgroup13/Controller/TableManagementController.java
@@ -1,11 +1,15 @@
 package com.gic_coffee_and_bakery.softwareeginerringgroup13.Controller;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.gic_coffee_and_bakery.softwareeginerringgroup13.DBManagement.TableManagement;
@@ -22,10 +26,27 @@ public class TableManagementController {
 	}
 
 	@PostMapping("/tablemanagement")
-	public ModelAndView login(@RequestParam("username") String username, 
-								@RequestParam("password") String password){
-								
-								}
+	public ModelAndView login(@RequestParam("table_id") int table_id, Model model){
+		
+		TableManagement tableManagement = new TableManagement();
+		tableManagement.disableTable(table_id);
+
+		model.addAttribute("allTable", displayTable());
+		return new ModelAndView("table_management");
+	
+	}
+
+	@GetMapping("/tablemanagement/addtable")
+	public ModelAndView handleDataFromView(Model model){
+		
+		TableManagement tableManagement = new TableManagement();
+		Table table = new Table(0, tableManagement.getMaxNumberOfNumber() + 1, "Available");
+		tableManagement.addTable(table);
+
+		model.addAttribute("allTable", displayTable());
+		return new ModelAndView("redirect:/tablemanagement");
+	
+	}
 
 
 
@@ -33,12 +54,7 @@ public class TableManagementController {
 		TableManagement tableManagement = new TableManagement();
 		return tableManagement.getAllTables();
 	}
-	 
-	private void createNewTable(int tableNumber) {
-		Table table = new Table(0, tableNumber, "Available");
-		TableManagement tableManagement = new TableManagement();
-		tableManagement.addTable(table);
-	}
 
+	
 }
 
--- src/main/java/com/gic_coffee_and_bakery/softwareeginerringgroup13/DBManagement/TableManagement.java
+++ src/main/java/com/gic_coffee_and_bakery/softwareeginerringgroup13/DBManagement/TableManagement.java
@@ -13,7 +13,7 @@ public class TableManagement extends Management<Table> {
         Table table = new Table();
         table.setId(rs.getInt("table_id"));
         table.setTableNumber(rs.getInt("tableNumber"));
-        table.setTableStatus(rs.getString("table_statue"));
+        table.setTableStatus(rs.getString("table_status"));
         return table;
     }
         
@@ -29,17 +29,17 @@ public class TableManagement extends Management<Table> {
     }
 
     public int addTable(Table table) {
-        String query = "INSERT INTO `table` (tableNumber, table_statue) VALUES (?, ?)";
+        String query = "INSERT INTO `table` (tableNumber, table_status) VALUES (?, ?)";
         return add(table, query);
     }
 
     public void updateTable(Table table) {
-        String query = "UPDATE `table` SET tableNumber = ?, table_statue = ? WHERE table_id = ?";
+        String query = "UPDATE `table` SET tableNumber = ?, table_status = ? WHERE table_id = ?";
         update(table, query);
     }
 
     public List<Table> getAllTables() {
-        String query = "SELECT * FROM `table`";
+        String query = "SELECT * FROM `table` WHERE table_status <> 'Disabled'";
         return getAll(query);
     }
 
@@ -52,4 +52,27 @@ public class TableManagement extends Management<Table> {
         String query = "DELETE FROM `table` WHERE table_id = ?";
         delete(tableId, query);
     }
+
+    public void disableTable(int table_id) {
+        String query = "UPDATE `table` SET table_status = 'Disabled' WHERE table_id = ?";
+        disable(table_id, query);
+    }
+    public int getMaxNumberOfNumber() {
+        String query = "SELECT MAX(tableNumber) AS maxTableNumber FROM `table`";
+        int maxTableNumber = 0;
+
+        try (PreparedStatement stmt = getConnection().prepareStatement(query);
+             ResultSet rs = stmt.executeQuery()) {
+
+            if (rs.next()) {
+                maxTableNumber = rs.getInt("maxTableNumber");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return maxTableNumber;
+    }
+
+
 }
